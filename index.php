ca3e1ee31ca6f819cd1440927c5386aec5e0a27f
anoops007 <anoopp.ssuresh@gmail.com>
2017-02-03 11:47:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,5 +25,6 @@ $options=array(
 	'header'=>"Content-Type: application/json\n"
 	)
 );
-
+$context=stream_context_create($options);
+file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
 
