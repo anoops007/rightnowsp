b04279a94436e10a7cced9d4bdc4f8d0bb8fa851
anoops007 <anoopp.ssuresh@gmail.com>
2017-02-07 12:36:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($hub_verify_token === $verify_token) {
 file_put_contents("fb.txt",file_get_contents("php://input"));
 
 $fb=file_get_contents("fb.txt");
-
+echo $fb;
 $input = json_decode($fb, true);
 
 
