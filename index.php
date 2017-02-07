f07f74dfb8b58bdf6b7a6fccf01c7937cb2f83cf
anoops007 <anoopp.ssuresh@gmail.com>
2017-02-07 12:04:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 //echo "hi";
 
 $access_token = "EAAJa5MZBCPRYBAEKVRnxyN60Bbw4TzUTJCQLrnaOcS2Wt5oS6khZBCBcp6uybTEzPbsnDzlgCd6MVjoiL4JeE0ZAxU79gJvJn8236TUZBpvZBNzGD7HfzPvPZBZBEQP3UI18bXZAGz6Hy9s9aUOZA7VNlE9g8fiAtqzOqRKBZARHi3YwZDZD";
-echo $access_token;
+//echo $access_token;
 $verify_token = "vijay";
 $hub_verify_token = null;
+echo $_REQUEST['hub_challenge'];
+
 if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
     $hub_verify_token = $_REQUEST['hub_verify_token'];
