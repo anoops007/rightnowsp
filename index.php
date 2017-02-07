9d1d5326c469e72d57d2b88e136057aa00cda034
anoops007 <anoopp.ssuresh@gmail.com>
2017-02-07 12:00:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "hi";
+
 $access_token = "EAAJa5MZBCPRYBAEKVRnxyN60Bbw4TzUTJCQLrnaOcS2Wt5oS6khZBCBcp6uybTEzPbsnDzlgCd6MVjoiL4JeE0ZAxU79gJvJn8236TUZBpvZBNzGD7HfzPvPZBZBEQP3UI18bXZAGz6Hy9s9aUOZA7VNlE9g8fiAtqzOqRKBZARHi3YwZDZD";
 
 $verify_token = "vijay";
@@ -12,9 +14,54 @@ if ($hub_verify_token === $verify_token) {
     echo $challenge;
 }
 
-//file_put_contents("fb.txt",file_get_contents("php://input"));
+
+file_put_contents("fb.txt",file_get_contents("php://input"));
 
 $fb=file_get_contents("fb.txt");
-echo $fb;
+echo '<pre>';
 $input = json_decode($fb, true);
 print_r($input);
+
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$message_to_reply = '';
+/**
+ * Some Basic rules to validate incoming messages
+ */
+if(preg_match('[time|current time|now]', strtolower($message))) {
+    // Make request to Time API
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $message_to_reply = $result;
+    }
+} else {
+    $message_to_reply = 'Huh! what do you mean?';
+}
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+//Initiate cURL.
+$ch = curl_init($url);
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
+}
+
